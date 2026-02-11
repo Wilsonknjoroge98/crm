4c26b18623cb38e547638b7aa5a2f51838a3de9e
Noah-Wilhelm <121883489+Noah-Wilhelm@users.noreply.github.com>
2026-02-11 17:37:16 +0100

    Resolved all comments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,9 +15,8 @@ import { createAppTheme } from './utils/theme';
 
 import useAuth from './hooks/useAuth';
 import { useEffect, useMemo } from 'react';
-import { initAuth } from './utils/initAuth';
 
-initAuth();
+
 // import Maintenance from './views/Maintenance';
 const App = () => {
   // return <Maintenance />;
